3a2d551c5d310dda65a6c95cc23f60e280f98970
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-04-26 00:23:39 -0700

    be able to display words

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ $.get('/api/meanings', function(data) {
 // Fetch data of words from server
 $.get('/api/words', function(data) {
     var wordsArray = JSON.parse(data);
-    window.LEXICON_DATA.playlists = wordsArray.words;
+    window.LEXICON_DATA.words = wordsArray.words;
     wordsLoaded = true;
     if (meaningsLoaded == true) {
         runApplication();
@@ -24,4 +24,19 @@ $.get('/api/words', function(data) {
 
 function runApplication() {
     console.log(window.LEXICON_DATA);
+    loadWords();
+}
+
+function loadWords() {
+    for (var i = 0; i < window.LEXICON_DATA.words.length; i++) {
+
+        var option_words = document.getElementById("optionWords");
+        var option_word = document.createElement("div");
+        option_word.className = "option_word";
+        option_word.id = "word" + window.LEXICON_DATA.words[i].id;
+        var content = document.createTextNode(window.LEXICON_DATA.words[i].spelling);
+
+        option_words.appendChild(option_word);
+        option_word.appendChild(content);
+    }
 }
